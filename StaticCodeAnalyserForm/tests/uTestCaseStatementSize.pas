22bb978aa5699d325c4a1be05ccf3c2a00c54b05
Nicolas Gerlach <nicolas.gerlach@gmx.de>
2026-08-07 19:29:05 +0200

    fix(sca): CaseStatementSize blankt String-Literale - SQL-CASE erzeugte Phantom-Funde
    Der Scanner lief auf StripFileCommentsKeepStrings: 'CASE WHEN ... END'
    in einem SQL-Literal startete einen Phantom-case, und dessen 'END'
    schloss zusaetzlich echte Pascal-case-Bloecke vorzeitig (falsche
    Branch-Zahlen in beide Richtungen). Fix: TDetectorUtils.
    BlankStringLiterals (laengenerhaltend, LineFor bleibt gueltig) vor der
    Keyword-Suche; 2 Regressionstests (Review-HIGH 4/6).
    
    A/B-Erwartung: SCA059-Drops (Phantom-cases in SQL-lastigen Units) und
    moegliche Adds (vorher vorzeitig zerschnittene echte cases).

--- StaticCodeAnalyserForm/tests/uTestCaseStatementSize.pas
+++ StaticCodeAnalyserForm/tests/uTestCaseStatementSize.pas
@@ -12,6 +12,8 @@ type
     [Test] procedure SmallCase_NoFinding;
     [Test] procedure LargeCase_Reported;
     [Test] procedure CaseStatementSize_KindAndSeverity;
+    [Test] procedure SqlLiteralCase_NoPhantomFinding;
+    [Test] procedure LiteralEndBeforeRealCase_StillReported;
   end;
 
 implementation
@@ -82,6 +84,46 @@ begin
       end;
     Assert.Fail('expected fkCaseStatementSize finding');
   finally Findings.Free; end;
+end;
+
+procedure TTestCaseStatementSize.SqlLiteralCase_NoPhantomFinding;
+// Review-HIGH 2026-08-08: 'CASE WHEN ... END' in einem SQL-Literal
+// startete einen Phantom-case - Literale werden jetzt geblankt.
+const SRC =
+  'unit t; implementation'#13#10 +
+  'procedure Foo;'#13#10 +
+  'begin'#13#10 +
+  '  Q.SQL.Text := ''SELECT CASE WHEN a=1 THEN 1 WHEN a=2 THEN 2'' +'#13#10 +
+  '    '' WHEN a=3 THEN 3 WHEN a=4 THEN 4 WHEN a=5 THEN 5 WHEN a=6'' +'#13#10 +
+  '    '' THEN 6 WHEN a=7 THEN 7 WHEN a=8 THEN 8 WHEN a=9 THEN 9'' +'#13#10 +
+  '    '' WHEN b=1 THEN 10 WHEN b=2 THEN 11 ELSE 0 END FROM t'';'#13#10 +
+  'end;';
+var F: TObjectList<TLeakFinding>;
+begin
+  F := TFindingHelper.FindingsOfFile(SRC);
+  try Assert.AreEqual<Integer>(0, TFindingHelper.Count(F, fkCaseStatementSize));
+  finally F.Free; end;
+end;
+
+procedure TTestCaseStatementSize.LiteralEndBeforeRealCase_StillReported;
+// Ein 'END' in einem Literal darf das ECHTE case nicht vorzeitig
+// schliessen - das grosse case danach muss weiter gemeldet werden.
+const SRC =
+  'unit t; implementation'#13#10 +
+  'procedure Foo; var X: Integer;'#13#10 +
+  'begin'#13#10 +
+  '  Log(''outer CASE x END marker'');'#13#10 +
+  '  case X of'#13#10 +
+  '    1: A1;  2: A2;  3: A3;  4: A4;  5: A5;'#13#10 +
+  '    6: A6;  7: A7;  8: A8;  9: A9; 10: A10;'#13#10 +
+  '   11: A11;'#13#10 +
+  '  end;'#13#10 +
+  'end;';
+var F: TObjectList<TLeakFinding>;
+begin
+  F := TFindingHelper.FindingsOfFile(SRC);
+  try Assert.AreEqual<Integer>(1, TFindingHelper.Count(F, fkCaseStatementSize));
+  finally F.Free; end;
 end;
 
 initialization
