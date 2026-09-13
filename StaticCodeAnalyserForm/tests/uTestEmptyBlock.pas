14d0af65e6a2c139e8f68e2c351b535f55d18826
Nicolas Gerlach <nicolas.gerlach@gmx.de>
2026-09-14 01:58:11 +0200

    test(SCA077): drei Starter und der var-Rueckwaertslauf waren ungetestet (UNGEBAUT)
    Posten 199. Getestet war nur der if/then-Kontext. IsRoutineBody kennt
    mehrere Starter, und while, else und case waren allesamt unbelegt.
    
    Vier Tests, alle am gebauten Stand gemessen:
      while C do begin end;            1
      if C then A else begin end;      1
      case K of 1: begin end; end;     1
      procedure Foo; var I: Integer; begin end;   0
    
    DER VIERTE IST DER WICHTIGE. Er belegt den var-Section-Rueckwaertslauf:
    der Scanner laeuft von 'begin' aus rueckwaerts ueber die var-Sektion bis
    zum Routinenkopf, um einen leeren ROUTINENRUMPF von einem leeren BLOCK
    zu unterscheiden. Ohne diesen Lauf wuerde hier gemeldet - und niemand
    haette es bemerkt.
    
    Bewegung: 0 - nur Tests.
    
    Regel-ID im Betreff korrigiert: SCA035 -> SCA077. Die alte kam aus einem
    grep auf DETECTORS.md, dessen erster Treffer in einer ANDEREN Tabelle
    stand. Autoritativ ist die Unit-Spalte der Katalogtabelle; das prueft
    jetzt tools/rule_id_gate.py.

--- StaticCodeAnalyserForm/tests/uTestEmptyBlock.pas
+++ StaticCodeAnalyserForm/tests/uTestEmptyBlock.pas
@@ -16,6 +16,11 @@ type
     [Test] procedure TopLevelInitEmpty_NotReported;
     [Test] procedure EmptyBlock_KindAndSeverity;
     [Test] procedure LiteralBeginEnd_NotReported;
+    // Posten 199: while / else / case und der var-Rueckwaertslauf
+    [Test] procedure EmptyBlockAfterWhile_Reported;
+    [Test] procedure EmptyBlockAfterElse_Reported;
+    [Test] procedure EmptyBlockInCaseBranch_Reported;
+    [Test] procedure EmptyRoutineBodyAfterVarSection_NoFinding;
   end;
 
 implementation
@@ -24,6 +29,91 @@ uses
   System.SysUtils, System.Generics.Collections,
   uSCAConsts, uMethodd12,
   uTestFindingHelper;
+
+{ --- Posten 199: die uebrigen Starter der Keywordliste ----------- }
+//
+// Getestet war nur der if/then-Kontext. IsRoutineBody kennt aber
+// mehrere Starter, und 'while', 'else' und 'case' waren allesamt
+// unbelegt - ebenso der var-Section-Rueckwaertslauf, der einen
+// LEEREN ROUTINENRUMPF von einem leeren Block unterscheidet.
+//
+// Alle vier am gebauten Stand gemessen.
+
+procedure TTestEmptyBlock.EmptyBlockAfterWhile_Reported;
+// Gemessen: 1.
+const SRC =
+  'unit t; implementation'#13#10+
+  'procedure Foo;'#13#10+
+  'begin'#13#10+
+  '  while C do begin end;'#13#10+
+  'end;';
+var F: TObjectList<TLeakFinding>;
+begin
+  F := TFindingHelper.FindingsOfFile(SRC);
+  try
+    Assert.AreEqual<Integer>(1,
+      TFindingHelper.Count(F, fkEmptyBlock),
+      'ein leerer while-Rumpf ist ein leerer Block');
+  finally F.Free; end;
+end;
+
+procedure TTestEmptyBlock.EmptyBlockAfterElse_Reported;
+// Gemessen: 1.
+const SRC =
+  'unit t; implementation'#13#10+
+  'procedure Foo;'#13#10+
+  'begin'#13#10+
+  '  if C then A else begin end;'#13#10+
+  'end;';
+var F: TObjectList<TLeakFinding>;
+begin
+  F := TFindingHelper.FindingsOfFile(SRC);
+  try
+    Assert.AreEqual<Integer>(1,
+      TFindingHelper.Count(F, fkEmptyBlock),
+      'ein leerer else-Zweig ist ein leerer Block');
+  finally F.Free; end;
+end;
+
+procedure TTestEmptyBlock.EmptyBlockInCaseBranch_Reported;
+// Gemessen: 1.
+const SRC =
+  'unit t; implementation'#13#10+
+  'procedure Foo;'#13#10+
+  'begin'#13#10+
+  '  case K of 1: begin end; end;'#13#10+
+  'end;';
+var F: TObjectList<TLeakFinding>;
+begin
+  F := TFindingHelper.FindingsOfFile(SRC);
+  try
+    Assert.AreEqual<Integer>(1,
+      TFindingHelper.Count(F, fkEmptyBlock),
+      'ein leerer case-Zweig ist ein leerer Block');
+  finally F.Free; end;
+end;
+
+procedure TTestEmptyBlock.EmptyRoutineBodyAfterVarSection_NoFinding;
+// DIE ABGRENZUNG: ein leerer ROUTINENRUMPF ist kein leerer Block -
+// dafuer laeuft der Scanner von begin aus RUECKWAERTS ueber die
+// var-Sektion bis zum Routinenkopf. Genau dieser Rueckwaertslauf war
+// ungetestet; ohne ihn wuerde hier gemeldet. Gemessen: 0.
+const SRC =
+  'unit t; implementation'#13#10+
+  'procedure Foo;'#13#10+
+  'var I: Integer;'#13#10+
+  'begin end;';
+var F: TObjectList<TLeakFinding>;
+begin
+  F := TFindingHelper.FindingsOfFile(SRC);
+  try
+    Assert.AreEqual<Integer>(0,
+      TFindingHelper.Count(F, fkEmptyBlock),
+      'ein leerer Routinenrumpf hinter einer var-Sektion ist kein '
+      + 'leerer Block');
+  finally F.Free; end;
+end;
+
 
 procedure TTestEmptyBlock.NonEmptyBlock_NoFinding;
 const SRC =
